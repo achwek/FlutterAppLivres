c6c5c879abe6b8cd6ac5472c8849c44a7716ea3e
unknown <achwek.issawi@gmail.com>
2023-03-09 22:16:52 +0100

    first commit

---
+++ lib/ListBook.dart
@@ -0,0 +1,112 @@
+import 'Book.dart';
+
+List<Book> books = [
+  Book(
+      titre: 'Je t\'aime de la tête aux pieds',
+      auteur: 'Bernadette Rossetti-Shustak',
+      description:
+          "Un bambin et son ourson en peluche invitent les tout-petits à différents moments de la journée, alors qu'ils sont tantôt joyeux et folichons, tantôt tristes ou en colère. Ils les rassurent en leur démontrant qu'ils sont aimés de la tête aux pieds, peu importe leur humeur.",
+      image: 'book1.jpg',
+      id: 1,
+      prix: 16.70,
+      quantite: 1),
+  Book(
+      titre: 'Dix petits orteils',
+      auteur: 'Caroline Jayne Church',
+      description:
+          "Un nouvel album de Caroline Jayne Church, l'auteure des populaires Je t'aime de la tête aux pieds et Bonne nuit, je t'aime! au format idéal pour les tout-petits.L'auteure de renom crée un autre classique qui charmera les enfants et leurs parents : Bouche, nez, yeux, oreilles, bras, jambes et dix petits orteils : une jolie histoire à partager!",
+      image: 'book2.jpg',
+      id: 1,
+      prix: 14.50,
+      quantite: 1),
+  Book(
+      titre: 'Je suis un grand frère',
+      auteur: 'Caroline Jayne Church',
+      description:
+          "Partagez la joie que ressent un aîné à la naissance d'un bébé! La venue d'un nouveau-né provoque plusieurs changements et les enfants aînés ont besoin d'un peu plus de tendresse pour s'ajuster à leur nouveau milieu familial. Cette histoire aux illustrations délicates est idéale pour un frère ou une sœur aînée qui se prépare à accueillir un bébé.",
+      image: 'book3.jpg',
+      id: 1,
+      prix: 12.95,
+      quantite: 1),
+  Book(
+      titre: 'La rentrée de Groloulou',
+      auteur: 'Géraldine Collet',
+      description:
+          "Une collection d' histoires tendres et drôles sur les animaux préférés des enfants en format rigide pour partager un joli moment de lecture ! C' est un grand jour pour Groloulou : la rentrée des classes ! Et il veut y être avant tout le monde. ",
+      image: 'book4.jpg',
+      id: 1,
+      prix: 6.95,
+      quantite: 1),
+  Book(
+      titre: 'P\'tit loup:sauve la planéte',
+      auteur: 'Eléonore Thuillier',
+      description:
+          "À l’école, la maîtresse a expliqué aux élèves que la planète est en danger. P’tit Loup est un peu inquiet. Heureusement, Maman lui explique que nous pouvons faire beaucoup de choses pour prendre soin de notre environnement. Penser à éteindre les lumières, recycler ses déchets, économiser l’eau...",
+      image: 'book5.jpg',
+      id: 1,
+      prix: 9.20,
+      quantite: 1),
+  Book(
+      titre: 'Petit Lion joue au foot',
+      auteur: 'Claire Bertholet',
+      description:
+          "Illustré de photos originales et inédites, ce petit livre retrace la petite et la grande histoire de l'Olympique Lyonnais, le club de foot au doubl",
+      image: 'book6.jpg',
+      id: 1,
+      prix: 22.90,
+      quantite: 1),
+  Book(
+      titre: 'Un amour de lémurien',
+      auteur: 'Paul Ann Whitford',
+      description:
+          "Qui aime Petit Lémurien ? Maman le câline au pied du tamarinier, Papa lui donne à manger, Mamie lui fait un gros baiser, ses cousins se pressent pour jouer... Quoi qu'il arrive, Petit Lémurien est entouré d'amour toute la journée !",
+      image: 'book7.jpg',
+      id: 1,
+      prix: 14.95,
+      quantite: 1),
+  Book(
+      titre: 'La journee De Jeu JOJO',
+      auteur: 'Sujatha Lalgudin',
+      description:
+          "Pour Nica, dix-sept ans, l'heure est venue de tirer un trait sur son passé. Son rêve est sur le point de se réaliser : un couple est venu l'adopter. Adieu l'orphelinat et sa terrifiante légende du fabricant de larmes. Mais dans sa nouvelle maison, Nica n'est pas seule. Rigel, l'orphelin agité et mystérieux avec qui elle a grandi, a lui aussi été adopté. Et il compte bien faire de la nouvelle vie de Nica un enfer. Pour Nica et Rigel, la guerre est déclarée. Celle de la haine qu'ils se sont vouée... ou des sentiments qu'ils n'oseront jamais s'avouer ?",
+      image: 'book8.jpg',
+      id: 1,
+      prix: 19.00,
+      quantite: 1),
+  Book(
+      titre: 'Cabane Magiquee',
+      auteur: 'Mary Pope Osborne',
+      description:
+          "La Cabane magique est une série de romans créés par Mary Pope Osborne et illustrés par Philipe Masson. Marie-Hélène Delval en est la traductrice en français. ",
+      image: 'book9.jpg',
+      id: 1,
+      prix: 7.90,
+      quantite: 1),
+  Book(
+      titre: 'CHAMPIGNONS',
+      auteur: 'Lise Gravel',
+      description:
+          "Aller aux champignons ressemble « à une chasse au trésor organisée par la nature » ! Partant de son expérience personnelle, Elise Gravel évoque sa passion pour les champignons, « créatures étranges venues de l'espace ». ",
+      image: 'book10.jpg',
+      id: 1,
+      prix: 20.95,
+      quantite: 1),
+  Book(
+      titre: 'La vallée des Dinosaures',
+      auteur: 'Mary Pope Osborne',
+      description:
+          "Grâce à une cabane magique rencontrée lors d'une promenade, Tom et sa soeur Léa sont envoyés à l'époque des dinosaures et vont vivre de nombreuses aventures.",
+      image: 'book11.jpg',
+      id: 1,
+      prix: 21.90,
+      quantite: 1),
+  Book(
+      titre: 'La Revue Des Livres Pour Enfants',
+      auteur: 'Mary Pope Osborne',
+      description:
+          "a Joie par les livres, aussi connue sous le nom de Centre national du livre pour enfants, est un service français voué à la littérature d'enfance et de jeunesse",
+      image: 'book12.jpg',
+      id: 1,
+      prix: 6.99,
+      quantite: 1),
+];
